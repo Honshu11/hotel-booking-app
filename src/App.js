59eaf640ae052878c923486e16a96ac8e5c0ef33
Honshu11 <egonza67@yahoo.com>
2021-05-18 20:17:33 -0700

    added button and modal, but dont know if the modal pop up works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
+import { Modal } from "bootstrap";
+import React from "react";
 import "./App.css";
 
+// MAIN TAKEAWAYS FROM PROJECT:
+// For this project you'll be creating a simple hotel booking application. A user who wants to book at the hotel will fill out some basic form fields and will be shown a confirmation message.
+
+//TODO: Pressing the "confirm" button, a modal should pop up the screen confirming the user's selection.
+//TODO: Lastly, style the page to best of your ability.
+
+// The application should have three components:
+//  App.js
+//    -contains state.
+//  BookingForm.js
+//    -No state.
+//  ConfirmationModal.js
+//    -No state.
+
 function App() {
-  // TODO: add drop to nights, adults, rooms
-  // TODO: firstName, lastName, email in text inputs.
-  // take inputs and put them into React.state.
   return (
-    <div>
-      <header>
+    <div className="main-container">
+      <header className="main-header">
         <h1>Hotel Booking App</h1>
+        <p>"The best hotel app on the web" -George Washington, probably.</p>
       </header>
-      <form className="main-container">
-        <label for="destination">Destination:</label>
-        <input type="text" id="destination" value="Paris, Tokyo..etc" />
-        <label for="d-date">Departure Date:</label>
-        <input type="date" id="d-date" name="d-date" />
-        <label for="a-date">Arrival Date:</label>
-        <input type="date" id="a-date" name="a-date" />
+      <form className="main-form">
+        <div className="user-form-info">
+          <label for="fName">First Name:</label>
+          <input type="text" name="fName" id="fName" />
+          <label for="lName">Last Name:</label>
+          <input type="text" name="lName" id="lName" />
+          <label for="email">Email:</label>
+          <input type="text" name="email" id="email" />
+          <label for="address">Address:</label>
+          <input type="text" name="address" id="address" />
+        </div>
+        <div className="user-room-info">
+          <label for="rooms-select">Rooms:</label>
+          <select id="rooms-select">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          <label for="guests-select">Guests:</label>
+          <select id="guests-select">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+          </select>
+          <label for="night-select">Nights:</label>
+          <select id="night-select">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+          </select>
+        </div>
       </form>
-      <input type="submit" value="Submit" />
+      <button
+        className="btn btn-primary"
+        data-toggle="modal"
+        data-target="#modal-submit">Confirm</button>
+
+        
+      <div className="modal fade" id="modal-submit">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h3 className="modal-title"></h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
